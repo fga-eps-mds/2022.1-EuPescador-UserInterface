5d3c2261c641ae31b3877e6da73c133eb8c197c9
MauricioMachadoFF <mauriciofozmmff33@gmail.com>
2022-03-04 21:42:44 -0300

    render mini batches in fishData
    Co-authored-by: Lucas Macedo Barboza <Luckx98@users.noreply.github.com>
    Co-authored-by: Felipe Candido de Moura <felipecdmoura@users.noreply.github.com>

--- src/components/WikiFishList/index.tsx
+++ src/components/WikiFishList/index.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import { WikiFishCard, IFish, IFishLog } from "../WikiFishCard";
 import { FishCardList } from "./styles";
 
@@ -9,25 +9,55 @@ interface FishListProps {
 }
 
 export const WikiFishList = ({ fishData, type, handleNavigation }: FishListProps) => {
-
+    const state = {
+        data: [
+            fishData[0],
+            fishData[1],
+            fishData[2],
+            fishData[3],
+            fishData[4],
+            fishData[5],
+            fishData[6],
+            fishData[7],
+            fishData[8],
+            fishData[9],
+        ],
+    }
+    
+    const renderItem = ({ item, index } : {item: any, index: number}) => (
+        <>
+            {type === "fishLog" ?
+                (<WikiFishCard
+                    fishLog={item as IFishLog}
+                    cardFunction={() => { item._id ? handleNavigation(item._id) : handleNavigation(index.toString()) }}
+                />) :
+                (<WikiFishCard
+                    fishWiki={item as IFish}
+                    cardFunction={() => { handleNavigation(item._id) }}
+                />)
+            }
+        </>
+    );
+            
     return (
-        <FishCardList
-            data={fishData}
-            renderItem={({ item, index }) => (
-                <>
-                    {type === "fishLog" ?
-                        (<WikiFishCard
-                            fishLog={item as IFishLog}
-                            cardFunction={() => { item._id ? handleNavigation(item._id) : handleNavigation(index.toString()) }}
-                        />) :
-                        (<WikiFishCard
-                            fishWiki={item as IFish}
-                            cardFunction={() => { handleNavigation(item._id) }}
-                        />)
-                    }
-                </>
-            )}
-            keyExtractor={item => item._id}
-        />
+            <FishCardList
+                data={state.data}
+                // renderItem={({ item, index }) => (
+                //     <>
+                //         {type === "fishLog" ?
+                //             (<WikiFishCard
+                //                 fishLog={item as IFishLog}
+                //                 cardFunction={() => { item._id ? handleNavigation(item._id) : handleNavigation(index.toString()) }}
+                //             />) :
+                //             (<WikiFishCard
+                //                 fishWiki={item as IFish}
+                //                 cardFunction={() => { handleNavigation(item._id) }}
+                //             />)
+                //         }
+                //     </>
+                // )}
+                renderItem={renderItem}
+                keyExtractor={item => item._id}
+            />
     );
 }
