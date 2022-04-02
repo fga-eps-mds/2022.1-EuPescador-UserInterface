eaecbfb539214e0852638243159959c7e5f66e5a
Leticia <leticiards.04@gmail.com>
2022-04-02 11:42:09 -0300

    Adaptação para exportar txt

--- src/components/FishLogs/index.tsx
+++ src/components/FishLogs/index.tsx
@@ -113,7 +113,7 @@ export const FishLogs = (
   };
 
 
-  const saveFile = async (csvFile: string) => {
+  const saveFile = async (txtFile: string) => {
     setIsLoading(true);
     try {
       const res = await MediaLibrary.requestPermissionsAsync()
@@ -122,8 +122,8 @@ export const FishLogs = (
         let today = new Date();
         let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getHours() + "-" + today.getMinutes();
 
-        let fileUri = FileSystem.documentDirectory + `registros-${date}.csv`;
-        await FileSystem.writeAsStringAsync(fileUri, csvFile);
+        let fileUri = FileSystem.documentDirectory + `registros-${date}.txt`;
+        await FileSystem.writeAsStringAsync(fileUri, txtFile);
         const asset = await MediaLibrary.createAssetAsync(fileUri);
         await MediaLibrary.createAlbumAsync("euPescador", asset, false);
 
