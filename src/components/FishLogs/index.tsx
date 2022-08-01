dfefcb330437f0f879a051378f551ee585706b7e
Wictorgirardi <wictor.girardi@gmail.com>
2022-07-31 22:00:57 -0300

    fix: fixing offline logic for registers
    Co-authored-by: lucasgandac <lucasgandac@hotmail.com>
    Co-authored-by: andreabenf <andreabenf@gmail.com>

--- src/components/FishLogs/index.tsx
+++ src/components/FishLogs/index.tsx
@@ -1,8 +1,8 @@
-import React, { useEffect, useState } from 'react';
-import { ActivityIndicator, Alert } from 'react-native';
-import * as FileSystem from 'expo-file-system';
-import {CheckBox} from 'react-native-elements';
-import NetInfo from '@react-native-community/netinfo';
+import React, { useEffect, useState } from "react";
+import { ActivityIndicator, Alert } from "react-native";
+import * as FileSystem from "expo-file-system";
+import { CheckBox } from "react-native-elements";
+import NetInfo, { useNetInfo } from "@react-native-community/netinfo";
 import {
   ButtonView,
   Container,
@@ -28,13 +28,13 @@ import {
   SearchImage,
   BoldText,
   RegularText,
-} from './styles';
-import { GetAllFishLogs } from '../../services/fishLogService/getAllLogs';
-import { ExportFishLogs } from '../../services/fishLogService/exportFishLogs';
-import { FishLogCard, IFishLog } from '../FishLogCard';
-import { DraftButton } from '../DraftButton';
-import { FilterButton } from '../FilterButton';
-import AsyncStorage from '@react-native-async-storage/async-storage';
+} from "./styles";
+import { GetAllFishLogs } from "../../services/fishLogService/getAllLogs";
+import { ExportFishLogs } from "../../services/fishLogService/exportFishLogs";
+import { FishLogCard, IFishLog } from "../FishLogCard";
+import { DraftButton } from "../DraftButton";
+import { FilterButton } from "../FilterButton";
+import AsyncStorage from "@react-native-async-storage/async-storage";
 
 interface Props {
   token: string;
@@ -43,13 +43,12 @@ interface Props {
   filterQuery: any;
 }
 
-export const FishLogs = (
-  { token,
-    navigation,
-    filterQuery,
-    isAdmin,
-  }: Props
-) => {
+export const FishLogs = ({
+  token,
+  navigation,
+  filterQuery,
+  isAdmin,
+}: Props) => {
   const [fishLog, setFishLog] = useState<IFishLog[]>([]);
   const [isLoading, setIsLoading] = useState(true);
   const [exportList, setExportList] = useState<string[]>([]);
@@ -60,11 +59,11 @@ export const FishLogs = (
   const { StorageAccessFramework } = FileSystem;
 
   const loadFishesLogsOffline = async () => {
-    let allFishesLogs = await AsyncStorage.getItem('@eupescador/allFishesLogs');
+    let allFishesLogs = await AsyncStorage.getItem("@eupescador/allFishesLogs");
     if (allFishesLogs) {
       let fishesLogs = JSON.parse(allFishesLogs);
 
-      const newFishesLogs = await AsyncStorage.getItem('@eupescador/newfish');
+      const newFishesLogs = await AsyncStorage.getItem("@eupescador/newfish");
 
       if (newFishesLogs) {
         let fishesUnSave = [];
@@ -76,14 +75,16 @@ export const FishLogs = (
       }
       setFishLog(fishesLogs.reverse());
     }
-  }
+  };
 
   const getFishLogs = async () => {
     setIsLoading(true);
 
     try {
       let data = await GetAllFishLogs(token, filterQuery);
-      const offlineRegisterArray = await AsyncStorage.getItem('@eupescador/newfish');
+      const offlineRegisterArray = await AsyncStorage.getItem(
+        "@eupescador/newfish"
+      );
       let fishesInCache = [];
       if (offlineRegisterArray) {
         fishesInCache = JSON.parse(offlineRegisterArray);
@@ -92,7 +93,10 @@ export const FishLogs = (
           data.push(fishesInCache[i]);
         }
       }
-      await AsyncStorage.setItem('@eupescador/allFishesLogs', JSON.stringify(data));
+      await AsyncStorage.setItem(
+        "@eupescador/allFishesLogs",
+        JSON.stringify(data)
+      );
       setFishLog(data.reverse());
     } catch (error: any) {
       console.log(error);
@@ -102,26 +106,25 @@ export const FishLogs = (
 
   const getDrafts = async () => {
     setIsLoading(true);
-    const drafts = await AsyncStorage.getItem('drafts');
-    if (drafts)
-      setHasDraft(drafts != '[]');
-  }
+    const drafts = await AsyncStorage.getItem("drafts");
+    if (drafts) setHasDraft(drafts != "[]");
+  };
 
   const handleNavigationOnline = (id: string) => {
     navigation.navigate(
-      'FishLog' as never,
+      "FishLog" as never,
       {
         log_id: id,
-      } as never,
+      } as never
     );
   };
 
   const handleNavigationOffline = (fish: IFishLog) => {
     navigation.navigate(
-      'FishLog' as never,
+      "FishLog" as never,
       {
-        fish
-      } as never,
+        fish,
+      } as never
     );
   };
 
@@ -130,7 +133,7 @@ export const FishLogs = (
     if (value) {
       fishLog.forEach((item) => {
         if (!exportList.includes(item.id)) {
-          setExportList(arr => [...arr, item.id]);
+          setExportList((arr) => [...arr, item.id]);
         }
       });
     } else {
@@ -143,48 +146,70 @@ export const FishLogs = (
   };
 
   const handleAddLog = async () => {
-    navigation.navigate("NewFishLog" as never, {
-      isNewRegister: true,
-      name: "Novo Registro",
-    } as never);
+    navigation.navigate(
+      "NewFishLog" as never,
+      {
+        isNewRegister: true,
+        name: "Novo Registro",
+      } as never
+    );
   };
 
-
   const saveFile = async (csvFile: string) => {
-  setIsLoading(true);
-  try {
-    const res = await StorageAccessFramework.requestDirectoryPermissionsAsync();
+    setIsLoading(true);
+    try {
+      const res =
+        await StorageAccessFramework.requestDirectoryPermissionsAsync();
 
-    if (res.granted) {
-      let today = new Date();
-      let date = today.getFullYear() + '-' + (today.getMonth() + 1) + '-' + today.getDate() + '-' + today.getHours() + "-" + today.getMinutes();
-      let filename = `registros-${date}.txt`
-      let directoryUri = res.directoryUri;
-      await StorageAccessFramework.createFileAsync(directoryUri, filename, "application/txt")
-        .then(async(fileUri) => {
-          await FileSystem.writeAsStringAsync(fileUri, csvFile, { encoding: FileSystem.EncodingType.UTF8 });
-        })
-        .catch((e) => {
-          console.log(e);
-        });
+      if (res.granted) {
+        let today = new Date();
+        let date =
+          today.getFullYear() +
+          "-" +
+          (today.getMonth() + 1) +
+          "-" +
+          today.getDate() +
+          "-" +
+          today.getHours() +
+          "-" +
+          today.getMinutes();
+        let filename = `registros-${date}.txt`;
+        let directoryUri = res.directoryUri;
+        await StorageAccessFramework.createFileAsync(
+          directoryUri,
+          filename,
+          "application/txt"
+        )
+          .then(async (fileUri) => {
+            await FileSystem.writeAsStringAsync(fileUri, csvFile, {
+              encoding: FileSystem.EncodingType.UTF8,
+            });
+          })
+          .catch((e) => {
+            console.log(e);
+          });
 
-      handleExport();
-      Alert.alert("Exportar Registros", "Registro(s) exportado(s) com sucesso!", [
+        handleExport();
+        Alert.alert(
+          "Exportar Registros",
+          "Registro(s) exportado(s) com sucesso!",
+          [
+            {
+              text: "Ok",
+            },
+          ]
+        );
+      }
+    } catch (error: any) {
+      console.log(error);
+      Alert.alert("Exportar Registros", "Falha ao exportar registro(s)!", [
         {
           text: "Ok",
-        }
-      ])
+        },
+      ]);
     }
-  } catch (error: any) {
-    console.log(error);
-    Alert.alert("Exportar Registros", "Falha ao exportar registro(s)!", [
-      {
-        text: "Ok",
-      }
-    ])
-  }
-  setIsLoading(false);
-};
+    setIsLoading(false);
+  };
 
   const handleExportSelected = async () => {
     try {
@@ -196,17 +221,17 @@ export const FishLogs = (
       Alert.alert("Exportar Registros", "Falha ao exportar registros", [
         {
           text: "Ok",
-        }
-      ])
+        },
+      ]);
     }
   };
 
   const addExportList = (logId: string) => {
-    setExportList(arr => [...arr, logId]);
+    setExportList((arr) => [...arr, logId]);
   };
 
   const removeExportList = (logId: string) => {
-    setExportList(exportList.filter(item => item !== logId));
+    setExportList(exportList.filter((item) => item !== logId));
   };
 
   useEffect(() => {
@@ -235,131 +260,127 @@ export const FishLogs = (
             <FilterButton
               url={filterQuery}
               navigation={navigation}
-              screen='LogFilter'
+              screen="LogFilter"
             />
-            {
-              isAdmin ? (
-                <ButtonView>
-                  <ExportButton onPress={handleExport}>
-                    {
-                      isExportMode ? <>
-                        <DownloadIcon name="cancel" />
-                        <CancelButtonText >Cancelar</CancelButtonText>
-                      </>
-
-                        :
-                        <>
-                          <DownloadIcon name="file-download" />
-                          <ExportButtonText>Exportar Registros</ExportButtonText>
-                        </>
-
-                    }
-                  </ExportButton>
-                </ButtonView>
-              ) : <ButtonView>
-                <ExportButton onPress={handleAddLog}>
-                <DownloadIcon name="add" />
-                          <ExportButtonText>Criar Novo Registro</ExportButtonText>
+            {isAdmin ? (
+              <ButtonView>
+                <ExportButton onPress={handleExport}>
+                  {isExportMode ? (
+                    <>
+                      <DownloadIcon name="cancel" />
+                      <CancelButtonText>Cancelar</CancelButtonText>
+                    </>
+                  ) : (
+                    <>
+                      <DownloadIcon name="file-download" />
+                      <ExportButtonText>Exportar Registros</ExportButtonText>
+                    </>
+                  )}
                 </ExportButton>
               </ButtonView>
-            }
+            ) : (
+              <ButtonView>
+                <ExportButton onPress={handleAddLog}>
+                  <DownloadIcon name="add" />
+                  <ExportButtonText>Criar Novo Registro</ExportButtonText>
+                </ExportButton>
+              </ButtonView>
+            )}
           </OptionsView>
           <ExportAllView>
-            {
-              isExportMode ? <>
+            {isExportMode ? (
+              <>
                 {/* <CheckBox value={isCheck} onValueChange={selectAllFunction} /> */}
                 <CheckBox
                   checked={isCheck}
                   onPress={() => selectAllFunction(!isCheck)}
-                  checkedColor={'#00BBD4'}
+                  checkedColor={"#00BBD4"}
                   uncheckedColor={"black"}
                 />
                 <ExportAllText>Selecionar todos os registros</ExportAllText>
               </>
-                : null
-            }
+            ) : null}
           </ExportAllView>
-          {hasDraft ?
-            <DraftButton /> :
-            null
-          }
-          
-          {
-            fishLog.length ? (
-              <FishCardList
-                data={fishLog}
-                renderItem={({ item }) => (
-                  <FishLogCard
-                    selectAll={isCheck}
-                    fishLog={item}
-                    isHidden={!isExportMode}
-                    cardFunction={async () => {
-                      await NetInfo.fetch().then(status => {
-                        if (status.isConnected) {
-                          handleNavigationOnline(item.id);
-                        } else {
-                          handleNavigationOffline(item);
-                        }
-                      })
-                    }}
-                    selectFunction={() => {
-                      addExportList(item.id);
-                    }}
-                    deselectFunction={() => {
-                      removeExportList(item.id);
-                    }}
-                  />
-                )}
-                keyExtractor={item => item.id}
-              />
-            ) : (
-              filterQuery ? (
+          {hasDraft ? <DraftButton /> : null}
 
-                <NoResultContainer>
-                  <SearchImage source={require('../../assets/search.png')} />
-                  <BoldText>Não encontramos nada com os filtros utilizados</BoldText>
-                  <RegularText>
-                    Por favor, verifique sua pesquisa e tente novamente para obter
-                    resultados.
-                  </RegularText>
-                </NoResultContainer>
+          {fishLog.length ? (
+            <FishCardList
+              data={fishLog}
+              renderItem={({ item }) => (
+                <FishLogCard
+                  selectAll={isCheck}
+                  fishLog={item}
+                  isHidden={!isExportMode}
+                  cardFunction={async () => {
+                    await NetInfo.fetch().then((status) => {
+                      if (status.isConnected) {
+                        handleNavigationOnline(item.id);
+                      } else {
+                        handleNavigationOffline(item);
+                      }
+                    });
+                  }}
+                  selectFunction={() => {
+                    addExportList(item.id);
+                  }}
+                  deselectFunction={() => {
+                    removeExportList(item.id);
+                  }}
+                />
+              )}
+              keyExtractor={(item) => item.id}
+            />
+          ) : filterQuery ? (
+            <NoResultContainer>
+              <SearchImage source={require("../../assets/search.png")} />
+              <BoldText>
+                Não encontramos nada com os filtros utilizados
+              </BoldText>
+              <RegularText>
+                Por favor, verifique sua pesquisa e tente novamente para obter
+                resultados.
+              </RegularText>
+            </NoResultContainer>
+          ) : null}
 
-              ) : null
-            )
-          }
-
-          {isExportMode ?
+          {isExportMode ? (
             <ExportSelectedView>
-              <ExportSelectedButton disabled={!exportList.length} onPress={() => {
-                Alert.alert("Exportar Registros", "Você deseja exportar esses registros?", [
-                  {
-                    text: "Cancelar",
-                    style: "cancel"
-                  },
-                  {
-                    text: "Ok",
-                    onPress: () => handleExportSelected()
-                  }
-                ])
-              }
-              }>
+              <ExportSelectedButton
+                disabled={!exportList.length}
+                onPress={() => {
+                  Alert.alert(
+                    "Exportar Registros",
+                    "Você deseja exportar esses registros?",
+                    [
+                      {
+                        text: "Cancelar",
+                        style: "cancel",
+                      },
+                      {
+                        text: "Ok",
+                        onPress: () => handleExportSelected(),
+                      },
+                    ]
+                  );
+                }}
+              >
                 <ExportSelectedButtonView>
                   <ExportSelectedText>Exportar Selecionados</ExportSelectedText>
                   <DownloadIconBottom name="file-download" />
                 </ExportSelectedButtonView>
               </ExportSelectedButton>
             </ExportSelectedView>
-            : <AddButtonView>
+          ) : (
+            <AddButtonView>
               <AddLogButton onPress={handleAddLog}>
                 <AddLogView>
                   <AddIcon name="add" />
                 </AddLogView>
               </AddLogButton>
             </AddButtonView>
-          }
+          )}
         </>
-      )
-      }
-    </Container >
+      )}
+    </Container>
   );
 };
