edc62758e62b12e77b5e2f376967089dce623d31
André <andreabenf@gmail.com>
2022-08-10 17:17:42 -0300

    #3 Ajuste no tamanho de mb no async

--- src/screens/ViewFishLog/index.tsx
+++ src/screens/ViewFishLog/index.tsx
@@ -232,8 +232,8 @@ export const FishLog = ({ navigation, route }: any) => {
         }}
       >
            <Marker
-            coordinate={{latitude: lat !== null ? lat : 0.0,
-            longitude: lng !== null ? lng : 0.0}}
+            coordinate={{latitude: lat ? lat : 0.0,
+            longitude: lng ? lng : 0.0}}
             title='titulo'
             description='grupo'
          />
