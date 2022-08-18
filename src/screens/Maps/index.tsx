8742799afcbb0e095c7e99b4282d2eaffd48e03d
João Gabriel Antunes <jg.antunes.medeiros@gmail.com>
2022-08-18 19:36:30 -0300

    #7 corrigindo bug que impedia usuário de atualizar localização do peixe no registro de peixes.

--- src/screens/Maps/index.tsx
+++ src/screens/Maps/index.tsx
@@ -1,14 +1,16 @@
 import React, { useState } from 'react';
 import { LocationUsageInfoContainer, LocationUsageInfoText, LocationUsageInfoTitle, LocationUsageInfoView, Map, MapButtonsView, MapContainer, MapInfoView, MapInstructions, MapInstructionsText } from './styles';
-import { Marker, MapEvent, LatLng } from 'react-native-maps';
+import { Marker, MapEvent, LatLng, MarkerAnimated } from 'react-native-maps';
 import { DefaultButton } from '../../components/Button';
+import { Platform } from 'expo-modules-core';
 
 export const MyMap = ({ navigation, route }: any) => {
     const [mark, setMark] = useState<LatLng>({ latitude: route.params.latitude, longitude: route.params.longitude });
 
     const handleDrag = (e: MapEvent) => {
-        setMark(e.nativeEvent.coordinate);
+        setMark({latitude: e.nativeEvent.coordinate.latitude, longitude: e.nativeEvent.coordinate.longitude});
     }
+
     const handleConfirm = (latitude: number, longitude: number) => {
         route.params.latitude = latitude;
         route.params.longitude = longitude;
@@ -29,25 +31,23 @@ export const MyMap = ({ navigation, route }: any) => {
                     latitudeDelta: 0.0322,
                     longitudeDelta: 0.0121,
                 }}
-                onPress={handleDrag}
+                onPress={(e: MapEvent) => {e.stopPropagation(); handleDrag(e);}}
             >
                 <Marker coordinate={mark} />
             </Map>
-            <MapInfoView>
-                <MapInstructions>
-                    <MapInstructionsText>Clique no mapa para marcar o local onde pegou o peixe</MapInstructionsText>
-                </MapInstructions>
-                <LocationUsageInfoView>
-                    <LocationUsageInfoContainer>
-                        <LocationUsageInfoTitle>Por que precisamos da localização?</LocationUsageInfoTitle>
-                        <LocationUsageInfoText>A localização enviada será utilizada pelas unidades ambientais responsáveis para mapear a posição das espécies marítimas</LocationUsageInfoText>
-                    </LocationUsageInfoContainer>
-                    <MapButtonsView>
-                        <DefaultButton type="secondary" text="Cancelar" buttonFunction={() => { navigation.goBack() }} />
-                        <DefaultButton text="Confirmar" buttonFunction={() => handleConfirm(mark.latitude, mark.longitude)} />
-                    </MapButtonsView>
-                </LocationUsageInfoView>
-            </MapInfoView>
+            <MapInstructions>
+                <MapInstructionsText>Clique no mapa para marcar o local onde pegou o peixe</MapInstructionsText>
+            </MapInstructions>
+            <LocationUsageInfoView>
+                <LocationUsageInfoContainer>
+                    <LocationUsageInfoTitle>Por que precisamos da localização?</LocationUsageInfoTitle>
+                    <LocationUsageInfoText>A localização enviada será utilizada pelas unidades ambientais responsáveis para mapear a posição das espécies</LocationUsageInfoText>
+                </LocationUsageInfoContainer>
+                <MapButtonsView>
+                    <DefaultButton type="secondary" text="Cancelar" buttonFunction={() => { navigation.goBack() }} />
+                    <DefaultButton text="Confirmar" buttonFunction={() => handleConfirm(mark.latitude, mark.longitude)} />
+                </MapButtonsView>
+            </LocationUsageInfoView>
         </MapContainer>
     )
 }
