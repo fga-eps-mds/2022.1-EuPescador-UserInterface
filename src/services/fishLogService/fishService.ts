bbd5313e00b0175f2dd6a9aa3e2f088f002229d4
Wictorgirardi <wictor.girardi@gmail.com>
2022-09-09 16:17:38 -0300

    adicao e correcao de timeout na chamada

--- src/services/fishLogService/fishService.ts
+++ src/services/fishLogService/fishService.ts
@@ -3,6 +3,7 @@ const config = require('../../../config');
 
 const fishLogService = axios.create({
   baseURL: `https://fish-log-2022-1.herokuapp.com`,
+  timeout: 120000
 });
 
 export { fishLogService };
